18d2d6c40fb9d0fc0d48156cfce2af17a3e9b3ee
muhenge <mugunga5@gmail.com>
2025-03-06 19:28:33 +0200

    Remove themed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/header";
-import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,11 +31,11 @@ export default function RootLayout({
   return (
       <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+
         <Header />
         {/* Add padding to main to prevent content overlap */}
         <main className="pt-20 pb-16 sm:pb-0">{children}</main>
-      </ThemeProvider>
+
       </body>
       </html>
   );
